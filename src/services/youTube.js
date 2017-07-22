889c1339a35be1119afa118dcde56fc5c8769dc9
Hack Reactor Student <student@hackreactor.com>
2017-07-21 20:14:55 -0700

    begin  request

--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,15 +1,31 @@
 angular.module('video-player')
-.service('youTube', function(callback) {
+.service('youTube', function($http) {
   // TODO
-  $http({
-    method: 'GET',
-    url: '/someUrl'
-  }).then(function successCallback(data) {
-      // this callback will be called asynchronously
-      // when the response is available
-    callback(data.items);
-  }, function errorCallback(response) {
-      // called asynchronously if an error occurs
-      // or server returns response with an error status.
-  });
+  this.search = (query, callback) => {
+    $http({
+      method: 'GET',
+      url: 'https://www.googleapis.com/youtube/v3/search',
+    }).then(function successCallback(data) {
+        // this callback will be called asynchronously
+        // when the response is available
+      console.log('success');
+      callback(data.items);
+    }, function errorCallback(data) {
+      console.log('error');
+        // called asynchronously if an error occurs
+        // or server returns response with an error status.
+    });
+  };
 });
+
+// $http({
+//     method: 'GET',
+//     url: '/someUrl'
+//   }).then(function successCallback(data) {
+//       // this callback will be called asynchronously
+//       // when the response is available
+//     callback(data.items);
+//   }, function errorCallback(response) {
+//       // called asynchronously if an error occurs
+//       // or server returns response with an error status.
+//   });
